afc82dc8ae3572dd0f2e5b4f040378bbb351eeee
makeline <111859525+makeline@users.noreply.github.com>
2020-03-27 17:41:14 +0000

    OK

--- app/src/androidTest/java/org/aerovek/chartr/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/aerovek/chartr/ExampleInstrumentedTest.kt
@@ -11,4 +11,14 @@ import org.junit.Assert.*
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * See [testing documentation](http://d.android.com/to
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("org.aerovek.chartr", appContext.packageName)
+    }
+}
