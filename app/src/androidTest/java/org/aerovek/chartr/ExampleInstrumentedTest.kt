9814767bdf6496daccaa542405aa85d2533e2cad
makeline <111859525+makeline@users.noreply.github.com>
2020-03-27 06:03:11 +0000

    20

---
+++ app/src/androidTest/java/org/aerovek/chartr/ExampleInstrumentedTest.kt
@@ -0,0 +1,14 @@
+package org.aerovek.chartr
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/to
