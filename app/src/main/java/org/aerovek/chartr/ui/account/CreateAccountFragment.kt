088747c2242fb9ba0075123669e5de8cc99fc7fd
makeline <111859525+makeline@users.noreply.github.com>
2020-05-23 20:30:35 +0000

    fix twitter

--- app/src/main/java/org/aerovek/chartr/ui/account/CreateAccountFragment.kt
+++ app/src/main/java/org/aerovek/chartr/ui/account/CreateAccountFragment.kt
@@ -132,3 +132,152 @@ class CreateAccountFragment : BaseFragment() {
             setupBackPressListener { findNavController().navigateUp() }
 
             val menuHost: MenuHost = requireActivity()
+            menuHost.addMenuProvider(object: MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.toolbar_save, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    when (menuItem.itemId) {
+                        R.id.action_save -> {
+                            viewModel.save()
+                        }
+                    }
+                    return true
+                }
+            }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+            viewModel.showRequiredFieldsWarning.observe(viewLifecycleOwner) {
+                Snackbar.make(requireView(), getString(R.string.create_account_required_fields_message), Snackbar.LENGTH_SHORT).show()
+            }
+
+            viewModel.saveSuccessful.observe(viewLifecycleOwner) {
+                Snackbar.make(requireView(), "Profile submitted - status pending", Snackbar.LENGTH_SHORT).show()
+                findNavController().popBackStack(R.id.homeFragment, false)
+            }
+
+            viewModel.checkPermissions.observe(viewLifecycleOwner) {
+                if (permissionsGranted()) {
+                    showBottomSheet()
+                } else {
+                    askPermissions()
+                }
+            }
+
+            viewModel.usernameExists.observe(viewLifecycleOwner) {
+                Snackbar.make(requireView(), getString(R.string.username_exists), Snackbar.LENGTH_LONG).show()
+            }
+
+            viewModel.showNoBalanceMessage.observe(viewLifecycleOwner) {
+                showGenericDialog(
+                    requireContext(),
+                    DialogModel(
+                        title = null,
+                        message = R.string.network_fee_required_message,
+                        positive = R.string.ok,
+                        negative = R.string.cancel,
+                        positiveFun = { },
+                        negativeFun = { findNavController().popBackStack(R.id.homeFragment, false) },
+                        exitVisibility = View.VISIBLE
+                    ), dismissListener = null
+                )
+            }
+
+        }.root
+    }
+
+    private fun showBottomSheet() {
+        val mediaChooserFragment = MediaChooserFragment()
+        mediaChooserFragment.setDismissListener(object : MediaChooserDismissListener {
+            override fun onDismiss(chosenType: Int?) {
+                chosenType?.let {
+                    when (it) {
+                        MediaChooserFragment.TYPE_TAKE_PHOTO -> openCamera()
+                        MediaChooserFragment.TYPE_CHOOSE_FROM_LIBRARY -> openGallery()
+                    }
+                }
+            }
+        })
+
+        mediaChooserFragment.show(this@CreateAccountFragment.parentFragmentManager, "media_chooser_bottomsheet")
+    }
+
+    private fun openCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+            val photoFile = try {
+                createImageFile()
+            } catch (e: IOException) {
+                println("ERROR CREATING IMAGE FILE in EditAccountFragment:137 - ${e.printStackTrace()}")
+                null
+            }
+
+            photoFile?.also {
+                currentPhotoFile = it
+                cameraLauncher.launch(this)
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir = requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+
+        return File.createTempFile(timeStamp, ".jpg", storageDir).apply {
+            currentPhotoPath = this.absolutePath
+        }
+    }
+
+    private fun openGallery() {
+        val galleryIntent = Intent(MediaStore.INTENT_ACTION_MEDIA_SEARCH)
+        galleryIntent.action = Intent.ACTION_GET_CONTENT
+        galleryIntent.type = "image/*"
+        galleryLauncher.launch(Intent.createChooser(galleryIntent, "Select image")) //galleryIntent)
+    }
+
+
+    private fun askPermissions() {
+        val dialogMessage = if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            R.string.request_exteral_storage
+        } else {
+            R.string.request_camera_for_profile
+        }
+
+        showGenericDialog(
+            requireContext(),
+            DialogModel(
+                title = R.string.request_camera_title,
+                message = dialogMessage,
+                positive = R.string.ok,
+                negative = R.string.cancel,
+                positiveFun = {
+                    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                        permissionsLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    } else {
+                        permissionsLauncher.launch(Manifest.permission.CAMERA)
+                    }
+                },
+                negativeFun = { },
+                exitVisibility = View.VISIBLE
+            ), dismissListener = null
+        )
+
+    }
+
+    private fun permissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private const val MAX_BIO_CHARS = 2000
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    add(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
+}
