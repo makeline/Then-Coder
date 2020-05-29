bc30c7b57b7a539e8badb787b149cc8b69fd0ae9
makeline <111859525+makeline@users.noreply.github.com>
2020-05-29 18:51:07 +0000

    delete unnecessary code

--- app/src/main/java/org/aerovek/chartr/ui/adapterItems/MoreItem.kt
+++ app/src/main/java/org/aerovek/chartr/ui/adapterItems/MoreItem.kt
@@ -31,4 +31,14 @@ import org.aerovek.chartr.R
 import org.aerovek.chartr.databinding.MoreItemBinding
 import org.aerovek.chartr.ui.adapterItems.viewmodels.MoreItemViewModel
 
-class MoreItem(private val context: Context, private val viewModel: More
+class MoreItem(private val context: Context, private val viewModel: MoreItemViewModel, private val tapCallbacks: MoreItemTapCallbacks) : BindableItem<MoreItemBinding>() {
+    override fun bind(viewBinding: MoreItemBinding, position: Int) {
+        viewBinding.imageView.setImageDrawable(viewModel.imageDrawable)
+        viewBinding.title = viewModel.title
+        viewBinding.vm = viewModel
+        viewBinding.callback = tapCallbacks
+    }
+
+    override fun getLayout() = R.layout.more_item
+
+    override fun initializeViewBinding(view: View): MoreItemBinding
