e2a9c5526cf5db95a9cdc76fa4016472e49f258f
makeline <111859525+makeline@users.noreply.github.com>
2020-06-08 07:13:23 +0000

    modify about page

--- app/src/main/java/org/aerovek/chartr/ui/adapterItems/WalletTypeaheadWordsItem.kt
+++ app/src/main/java/org/aerovek/chartr/ui/adapterItems/WalletTypeaheadWordsItem.kt
@@ -29,4 +29,15 @@ import com.xwray.groupie.viewbinding.BindableItem
 import org.aerovek.chartr.R
 import org.aerovek.chartr.databinding.WalletTypeaheadWordsItemBinding
 
-class WalletTypeaheadWordsItem(val title: 
+class WalletTypeaheadWordsItem(val title: String, val tapCallback: StringItemTapCallback) : BindableItem<WalletTypeaheadWordsItemBinding>() {
+    override fun bind(viewBinding: WalletTypeaheadWordsItemBinding, position: Int) {
+        viewBinding.item = title
+        viewBinding.callback = tapCallback
+    }
+
+    override fun getLayout() = R.layout.wallet_typeahead_words_item
+
+    override fun initializeViewBinding(view: View): WalletTypeaheadWordsItemBinding {
+        return DataBindingUtil.bind(view)!!
+    }
+}
