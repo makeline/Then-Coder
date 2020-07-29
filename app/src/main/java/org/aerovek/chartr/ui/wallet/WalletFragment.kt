05d67318b29c8dc9c647981d168d19589e494253
makeline <111859525+makeline@users.noreply.github.com>
2020-07-29 12:38:05 +0000

    Actualizado

--- app/src/main/java/org/aerovek/chartr/ui/wallet/WalletFragment.kt
+++ app/src/main/java/org/aerovek/chartr/ui/wallet/WalletFragment.kt
@@ -22,3 +22,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 package org.aerovek.chartr.ui.wallet
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+im
