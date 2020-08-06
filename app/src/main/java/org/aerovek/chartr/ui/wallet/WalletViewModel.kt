9fc2f6f50643f19ef149c749f43ac4e9d818e4c8
makeline <111859525+makeline@users.noreply.github.com>
2020-08-06 14:52:56 +0000

    UI fixes

--- app/src/main/java/org/aerovek/chartr/ui/wallet/WalletViewModel.kt
+++ app/src/main/java/org/aerovek/chartr/ui/wallet/WalletViewModel.kt
@@ -25,4 +25,13 @@ package org.aerovek.chartr.ui.wallet
 
 import android.app.Application
 import android.content.SharedPreferences
-import andr
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.hadilq.liveevent.LiveEvent
+import org.aerovek.chartr.ui.AppConstants
+import org.aerovek.chartr.ui.BaseViewModel
+import org.aerovek.chartr.util.NavigationEvent
+
+class WalletViewModel(app: Application, sharedPreferences: SharedPreferences) : BaseViewModel(app) {
+    private val _navigationEvent = LiveEvent<NavigationEvent>()
+    val navigationEvent: LiveData<NavigationEvent> = _navi
