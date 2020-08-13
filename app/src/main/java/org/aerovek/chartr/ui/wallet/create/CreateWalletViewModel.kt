06879a77d5ddc189c788d63a4629907472021991
makeline <111859525+makeline@users.noreply.github.com>
2020-08-13 19:44:00 +0000

    testimonials updated via TUNE

--- app/src/main/java/org/aerovek/chartr/ui/wallet/create/CreateWalletViewModel.kt
+++ app/src/main/java/org/aerovek/chartr/ui/wallet/create/CreateWalletViewModel.kt
@@ -17,4 +17,17 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package org.aerovek.chartr.ui.wallet.create
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.hadilq.liveevent.LiveEvent
+import org.aerovek.chartr.ui.BaseViewModel
+import org.aerovek.chartr.util.NavigationEvent
+
+class CreateWalletViewModel(app: Application) : BaseViewModel(app) {
+    private val _navig
