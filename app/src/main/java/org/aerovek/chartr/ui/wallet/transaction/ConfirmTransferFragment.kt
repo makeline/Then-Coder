6837679ce79c84b4c957459eb066fd4c19e86d19
makeline <111859525+makeline@users.noreply.github.com>
2020-08-21 14:04:02 +0000

    Updated API docs

--- app/src/main/java/org/aerovek/chartr/ui/wallet/transaction/ConfirmTransferFragment.kt
+++ app/src/main/java/org/aerovek/chartr/ui/wallet/transaction/ConfirmTransferFragment.kt
@@ -46,3 +46,73 @@ class ConfirmTransferFragment : BottomSheetDialogFragment() {
     private var dismissListener: ConfirmTransferDismissListener? = null
     private var didCompleteTransaction: Boolean = false
     var assetBeingSent: String? = null
+
+    companion object {
+        fun newInstance() = ConfirmTransferFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return DataBindingUtil.inflate<ConfirmTransferFragmentBinding>(inflater, R.layout.confirm_transfer_fragment, container, false).apply {
+            binding = this
+            vm = viewModel
+            lifecycleOwner = viewLifecycleOwner
+
+            viewModel.closeButtonClicked.observe(viewLifecycleOwner) {
+                dismiss()
+            }
+
+            viewModel.showPinPad.observe(viewLifecycleOwner) {
+                val passcodeView = PassCodeFragment()
+                passcodeView.setup(object : PassCodeDismissListener {
+                    override fun onDismiss(isValidPin: Boolean) {
+                        if (isValidPin) {
+                            viewModel.sendTransaction()
+                        }
+                    }
+                }, true)
+
+                passcodeView.show(this@ConfirmTransferFragment.parentFragmentManager, "send_transaction_pin")
+            }
+
+            viewModel.transactionComplete.observe(viewLifecycleOwner) {
+                didCompleteTransaction = true
+                dismiss()
+            }
+
+            viewModel.transactionFailed.observe(viewLifecycleOwner) {
+                didCompleteTransaction = false
+                dismiss()
+            }
+        }.root
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.makeBottomsheetFullScreen(this, true)
+        return dialog
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        dismissListener?.onDismiss(didCompleteTransaction)
+    }
+
+    fun setup(dismissListener: ConfirmTransferDismissListener, aeroAmount: String, usdAmount: String, recipientAddress: String, asset: String) {
+        this.dismissListener = dismissListener
+        val truncatedAddress =
+            "${recipientAddress.substring(0, 9)}...${recipientAddress.substring(recipientAddress.length - 10)}"
+        val usd = usdAmount.substring(usdAmount.indexOf("~") + 1).trim()
+        viewModel.truncatedAddress.postValue(truncatedAddress)
+        viewModel.recipientAddress.postValue(recipientAddress)
+        viewModel.transferAmount.postValue("-$aeroAmount $asset")
+        viewModel.usdAmount.postValue(usd)
+        viewModel.initialize(aeroAmount, usdAmount, asset)
+    }
+}
+
+interface ConfirmTransferDismissListener {
+    fun onDismiss(didCompleteTransaction: Boolean)
+}
