b8f547b1e14fada2b1ba5ecf4c9b34dae8443def
makeline <111859525+makeline@users.noreply.github.com>
2020-10-09 20:20:35 +0000

    add yarn lock

--- app/src/main/java/org/aerovek/chartr/util/AutoCompleteTextViewBindingAdapters.kt
+++ app/src/main/java/org/aerovek/chartr/util/AutoCompleteTextViewBindingAdapters.kt
@@ -7,4 +7,13 @@ import androidx.databinding.BindingAdapter
 import com.google.android.material.textfield.TextInputLayout
 import org.aerovek.chartr.R
 
-// Need both to be handled in same function as it can happen that the initialSe
+// Need both to be handled in same function as it can happen that the initialSelection is called prior to setting the adapter
+@BindingAdapter(value = ["entries", "initialSelection"], requireAll = false)
+fun <T : Any> AutoCompleteTextView.setAdapterWithInitialSelection(entries: List<T>?, position: Int?) {
+    if (entries != null && position != null) {
+        setAdapterWithInitialSelectionWorker(entries, position)
+    }
+}
+
+// Need both to be handled in same function as it can happen that the initialSelection is called prior to setting the adapter
+@BindingAdapter(value = ["entries", "
