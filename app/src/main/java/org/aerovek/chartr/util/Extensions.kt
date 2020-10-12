1cceea7c54072cd66e1c88ea11af56fc51e1d721
makeline <111859525+makeline@users.noreply.github.com>
2020-10-12 21:43:33 +0000

    version 0.4.3

--- app/src/main/java/org/aerovek/chartr/util/Extensions.kt
+++ app/src/main/java/org/aerovek/chartr/util/Extensions.kt
@@ -26,4 +26,14 @@ package org.aerovek.chartr.util
 /**
  * Converts the big integer string representation of a token
  * balance into the decimal version derived by dividing
- * the big int by 1000000000000000000 which essentially moves the decimal over 
+ * the big int by 1000000000000000000 which essentially moves the decimal over 18 places to the left
+*/
+fun String.formatTokenBalance(decimalPrecision: Int? = null): String {
+    return if (this.isNotEmpty() && this != "0") {
+        val amountParts = this.toBigInteger().divideAndRemainder("1000000000000000000".toBigInteger())
+        val quotient = amountParts[0].toString()
+        var remainder = amountParts[1].toString()
+
+        if (remainder.length < 18) {
+            for (i in remainder.length..17) {
+                remainder = "
