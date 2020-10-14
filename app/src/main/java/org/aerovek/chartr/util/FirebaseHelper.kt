8a3799fd0e495a5b15906e2a92a1dbae5cabc05f
makeline <111859525+makeline@users.noreply.github.com>
2020-10-14 22:08:09 +0000

    lint.

--- app/src/main/java/org/aerovek/chartr/util/FirebaseHelper.kt
+++ app/src/main/java/org/aerovek/chartr/util/FirebaseHelper.kt
@@ -21,4 +21,21 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-package org.aerovek.c
+package org.aerovek.chartr.util
+
+import android.os.Bundle
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
+
+enum class FirebaseHelper(private val eventName: String) {
+    HandledException("handled_exception");
+
+    fun logEvent(payload: String) {
+        val bundle = Bundle()
+
+        // Firebase only allows 100 characters per event
+        if (payload.length > 100) {
+            println("[FirebaseHelper] PAYLOAD LENGTH WAS GREATER THAN 100, BAILING OUT!")
+            return
+        }
+
