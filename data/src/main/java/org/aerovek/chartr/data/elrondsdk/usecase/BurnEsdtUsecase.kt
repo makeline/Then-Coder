c50a50902c86986d65708d67a16ea2aa71de4b72
makeline <111859525+makeline@users.noreply.github.com>
2022-04-06 22:15:31 +0000

    userns: Don't allow setgroups until a gid mapping has been setablished

--- data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/BurnEsdtUsecase.kt
+++ data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/BurnEsdtUsecase.kt
@@ -1,1 +1,9 @@
-package org.aerovek.chartr.data.e
+package org.aerovek.chartr.data.elrondsdk.usecase
+
+import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants
+import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants.ESDT_MANAGEMENT_GAS_LIMIT
+import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants.ESDT_TRANSACTION_VALUE
+import org.aerovek.chartr.data.model.elrond.account.Account
+import org.aerovek.chartr.data.model.elrond.network.NetworkConfig
+import org.aerovek.chartr.data.model.elrond.transaction.Transaction
+import org.aerovek.chartr.data.model.e
