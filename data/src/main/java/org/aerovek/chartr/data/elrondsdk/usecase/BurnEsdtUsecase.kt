12abf59a38f5d0ef2efd95e457d23137aeb7fa31
makeline <111859525+makeline@users.noreply.github.com>
2022-04-07 11:03:24 +0000

    Added badge

--- data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/BurnEsdtUsecase.kt
+++ data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/BurnEsdtUsecase.kt
@@ -6,4 +6,20 @@ import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants.ESDT_TRANSACTION_
 import org.aerovek.chartr.data.model.elrond.account.Account
 import org.aerovek.chartr.data.model.elrond.network.NetworkConfig
 import org.aerovek.chartr.data.model.elrond.transaction.Transaction
-import org.aerovek.chartr.data.model.e
+import org.aerovek.chartr.data.model.elrond.wallet.Wallet
+import org.aerovek.chartr.data.util.toHex
+import java.math.BigInteger
+
+@Deprecated("DO NOT USE!!! This should be converted into its respective repository implementation")
+class BurnEsdtUsecase internal constructor(
+    private val sendTransactionUsecase: SendTransactionUsecase
+) {
+
+    fun execute(
+        account: Account,
+        wallet: Wallet,
+        networkConfig: NetworkConfig,
+        gasPrice: Long,
+        tokenIdentifier: String,
+        supplyToBurn: BigInteger
+    ): Transact
