732ec6614b6c5260cd27bc0a5b016908926eaafa
makeline <111859525+makeline@users.noreply.github.com>
2022-04-10 07:18:23 +0000

    Version bump to 0.2.5

--- data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/CallContractUsecase.kt
+++ data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/CallContractUsecase.kt
@@ -5,4 +5,16 @@ import org.aerovek.chartr.data.model.elrond.address.Address
 import org.aerovek.chartr.data.model.elrond.network.NetworkConfig
 import org.aerovek.chartr.data.model.elrond.transaction.Transaction
 import org.aerovek.chartr.data.model.elrond.wallet.Wallet
-import 
+import org.aerovek.chartr.data.util.ScUtils
+import java.math.BigInteger
+
+@Deprecated("DO NOT USE!!! This should be converted into its respective repository implementation")
+class CallContractUsecase internal constructor(
+    private val sendTransactionUsecase: SendTransactionUsecase,
+) {
+
+    // source:
+    // https://github.com/ElrondNetwork/elrond-sdk/blob/576fdc4bc0fa713738d8556600f04e6377c7623f/erdpy/contracts.py#L62
+    fun execute(
+        account: Account,
+        wallet: Wa
