92c8e77c398a443874c21d3b95bd47d1e188367f
makeline <111859525+makeline@users.noreply.github.com>
2022-04-13 09:41:50 +0000

    msm: mdss: fix ret value during error case

--- data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/ChangeOwnerEsdtUsecase.kt
+++ data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/ChangeOwnerEsdtUsecase.kt
@@ -2,4 +2,12 @@ package org.aerovek.chartr.data.elrondsdk.usecase
 
 import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants.ESDT_MANAGEMENT_GAS_LIMIT
 import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants.ESDT_TRANSACTION_VALUE
-i
+import org.aerovek.chartr.data.model.elrond.account.Account
+import org.aerovek.chartr.data.model.elrond.address.Address
+import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants
+import org.aerovek.chartr.data.model.elrond.network.NetworkConfig
+import org.aerovek.chartr.data.model.elrond.transaction.Transaction
+import org.aerovek.chartr.data.model.elrond.wallet.Wallet
+import org.aerovek.chartr.data.util.toHex
+
+@Deprecated("DO NOT USE!!! This should be converted into its respective re
