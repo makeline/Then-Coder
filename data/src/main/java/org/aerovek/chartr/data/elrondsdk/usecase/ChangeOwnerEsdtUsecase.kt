acb155eef0914354e92555e814ecf9acb05c0704
makeline <111859525+makeline@users.noreply.github.com>
2022-04-14 08:16:30 +0000

    Add pipeline-sample for an example project

--- data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/ChangeOwnerEsdtUsecase.kt
+++ data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/ChangeOwnerEsdtUsecase.kt
@@ -10,4 +10,21 @@ import org.aerovek.chartr.data.model.elrond.transaction.Transaction
 import org.aerovek.chartr.data.model.elrond.wallet.Wallet
 import org.aerovek.chartr.data.util.toHex
 
-@Deprecated("DO NOT USE!!! This should be converted into its respective re
+@Deprecated("DO NOT USE!!! This should be converted into its respective repository implementation")
+class ChangeOwnerEsdtUsecase internal constructor(
+    private val sendTransactionUsecase: SendTransactionUsecase
+) {
+
+    fun execute(
+        account: Account,
+        wallet: Wallet,
+        networkConfig: NetworkConfig,
+        gasPrice: Long,
+        tokenIdentifier: String,
+        newOwnerAddress: Address
+    ): Transaction {
+        val args = mutableListOf(
+            tokenIdentifier.toHex(),
+            newOwnerAddress.hex
+        )
+      
