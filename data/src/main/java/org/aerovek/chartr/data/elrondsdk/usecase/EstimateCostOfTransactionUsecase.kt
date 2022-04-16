f22e574df83520254f9a0a9a5a862cbfaf729aff
makeline <111859525+makeline@users.noreply.github.com>
2022-04-16 11:19:11 +0000

    Create translate.lua

--- data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/EstimateCostOfTransactionUsecase.kt
+++ data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/EstimateCostOfTransactionUsecase.kt
@@ -4,4 +4,8 @@ import org.aerovek.chartr.data.model.elrond.transaction.Transaction
 import org.aerovek.chartr.data.repository.elrond.TransactionRepository
 
 @Deprecated("DO NOT USE!!! This should be converted into its respective repository implementation")
-class EstimateCostOfTransactionUsecase internal constructor(private val transactionRepository: TransactionRepos
+class EstimateCostOfTransactionUsecase internal constructor(private val transactionRepository: TransactionRepository) {
+
+    fun execute(transaction: Transaction) =
+        transactionRepository.estimateCostOfTransaction(transaction)
+}
---
+++ data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/FreezeAccountEsdtUsecase.kt
@@ -0,0 +1,7 @@
+package org.aerovek.chartr.data.elrondsdk.usecase
+
+import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants.ESDT_MANAGEMENT_GAS_LIMIT
+import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants.ESDT_TRANSACTION_VALUE
+import org.aerovek.chartr.data.model.elrond.account.Account
+import org.aerovek.chartr.data.model.elrond.address.Address
+import org.aerovek.chartr.data.model.elro
