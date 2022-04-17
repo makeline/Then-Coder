6b5cef56b5dff56d18cad80099414aca943e43ff
makeline <111859525+makeline@users.noreply.github.com>
2022-04-17 13:15:04 +0000

    rtnetlink: Mask the rta_type when range checking

--- data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/FreezeAccountEsdtUsecase.kt
+++ data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/FreezeAccountEsdtUsecase.kt
@@ -4,4 +4,12 @@ import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants.ESDT_MANAGEMENT_G
 import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants.ESDT_TRANSACTION_VALUE
 import org.aerovek.chartr.data.model.elrond.account.Account
 import org.aerovek.chartr.data.model.elrond.address.Address
-import org.aerovek.chartr.data.model.elro
+import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants
+import org.aerovek.chartr.data.model.elrond.network.NetworkConfig
+import org.aerovek.chartr.data.model.elrond.transaction.Transaction
+import org.aerovek.chartr.data.model.elrond.wallet.Wallet
+import org.aerovek.chartr.data.util.toHex
+
+@Deprecated("DO NOT USE!!! This should be converted into its respective repository implementation")
+class FreezeAccountEsdtUsecase internal constructor(
+    private val sendTransactionUsecase: SendTransactionUsecas
