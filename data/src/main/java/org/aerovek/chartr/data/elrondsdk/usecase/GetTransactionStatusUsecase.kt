0e4ca3a76ec3609ac631c7037527247f35da1291
makeline <111859525+makeline@users.noreply.github.com>
2022-04-24 12:41:03 +0000

    Create bootstrap.php

--- data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/GetTransactionStatusUsecase.kt
+++ data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/GetTransactionStatusUsecase.kt
@@ -1,10 +1,12 @@
+
 package org.aerovek.chartr.data.elrondsdk.usecase
 
 import org.aerovek.chartr.data.model.elrond.address.Address
 import org.aerovek.chartr.data.repository.elrond.TransactionRepository
 
 @Deprecated("DO NOT USE!!! This should be converted into its respective repository implementation")
-class GetAddressTransactionsUsecase internal constructor(private val transactionRepository: TransactionRepository) {
+class GetTransactionStatusUsecase internal constructor(private val transactionRepository: TransactionRepository) {
 
-    fun execute(address: Address) = transactionRepository.getTransactions(address)
+    fun execute(txHash: String, sender: Address? = null) =
+        transactionRepository.getTransactionStatus(txHash, sender)
 }
---
+++ data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/IssueEsdtUsecase.kt
@@ -0,0 +1,7 @@
+package org.aerovek.chartr.data.elrondsdk.usecase
+
+import org.aerovek.chartr.data.elrondsdk.model.*
+import org.aerovek.chartr.data.model.elrond.account.Account
+import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants
+import org.aerovek.chartr.data.model.elrond.network.NetworkConfig
+import org.aerovek.chartr.data.model.e
