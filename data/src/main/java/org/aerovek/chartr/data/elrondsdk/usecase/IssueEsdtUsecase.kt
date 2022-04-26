c050985a3c2e9fcccfe05a9a6e55dee847cf3861
makeline <111859525+makeline@users.noreply.github.com>
2022-04-26 10:26:37 +0000

    minor UI changes

--- data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/IssueEsdtUsecase.kt
+++ data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/IssueEsdtUsecase.kt
@@ -4,4 +4,17 @@ import org.aerovek.chartr.data.elrondsdk.model.*
 import org.aerovek.chartr.data.model.elrond.account.Account
 import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants
 import org.aerovek.chartr.data.model.elrond.network.NetworkConfig
-import org.aerovek.chartr.data.model.e
+import org.aerovek.chartr.data.model.elrond.transaction.Transaction
+import org.aerovek.chartr.data.model.elrond.wallet.Wallet
+import org.aerovek.chartr.data.util.ScUtils
+import org.aerovek.chartr.data.util.toHex
+import org.aerovek.chartr.data.util.toHexString
+import java.math.BigInteger
+
+@Deprecated("DO NOT USE!!! This should be converted into its respective repository implementation")
+class IssueEsdtUsecase internal constructor(
+    private val sendTransactionUsecase: SendTransactionUsecase
+) {
+
+    fun execute(
+        account: Accou
