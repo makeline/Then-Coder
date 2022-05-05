ffe08afdeeb789a18e1377ea8d00fd2683748527
makeline <111859525+makeline@users.noreply.github.com>
2022-05-05 12:54:31 +0000

    Added missing images

--- data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/PauseAccountEsdtUsecase.kt
+++ data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/PauseAccountEsdtUsecase.kt
@@ -4,4 +4,16 @@ import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants
 import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants.ESDT_MANAGEMENT_GAS_LIMIT
 import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants.ESDT_TRANSACTION_VALUE
 import org.aerovek.chartr.data.model.elrond.account.Account
-impor
+import org.aerovek.chartr.data.model.elrond.network.NetworkConfig
+import org.aerovek.chartr.data.model.elrond.transaction.Transaction
+import org.aerovek.chartr.data.model.elrond.wallet.Wallet
+import org.aerovek.chartr.data.util.toHex
+
+@Deprecated("DO NOT USE!!! This should be converted into its respective repository implementation")
+class PauseAccountEsdtUsecase internal constructor(
+    private val sendTransactionUsecase: SendTransactionUsecase
+) {
+
+    fun execute(
+        account: Account,
+        wall
