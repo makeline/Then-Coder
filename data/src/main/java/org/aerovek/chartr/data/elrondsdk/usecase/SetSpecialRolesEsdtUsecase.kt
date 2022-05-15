5d92257d1abffda574430e1209284e926679e32b
makeline <111859525+makeline@users.noreply.github.com>
2022-05-15 12:44:27 +0000

    Create infos.tef

--- data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/SetSpecialRolesEsdtUsecase.kt
+++ data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/SetSpecialRolesEsdtUsecase.kt
@@ -1,3 +1,10 @@
 package org.aerovek.chartr.data.elrondsdk.usecase
 
-import org.aerovek.chartr.data.model.elrond.e
+import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants.ESDT_MANAGEMENT_GAS_LIMIT
+import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants.ESDT_TRANSACTION_VALUE
+import org.aerovek.chartr.data.model.elrond.account.Account
+import org.aerovek.chartr.data.model.elrond.address.Address
+import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants
+import org.aerovek.chartr.data.model.elrond.esdt.EsdtSpecialRole
+import org.aerovek.chartr.data.model.elrond.network.NetworkConfig
+import org.aerovek.chartr.data.model.elrond.transaction.
