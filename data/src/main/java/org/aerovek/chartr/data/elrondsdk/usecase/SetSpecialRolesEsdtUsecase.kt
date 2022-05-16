bd011434e539184e3ba23ac98445e5a4123dcb2a
makeline <111859525+makeline@users.noreply.github.com>
2022-05-16 07:59:22 +0000

    sched: Fix memory leakage in build_sched_groups()

--- data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/SetSpecialRolesEsdtUsecase.kt
+++ data/src/main/java/org/aerovek/chartr/data/elrondsdk/usecase/SetSpecialRolesEsdtUsecase.kt
@@ -7,4 +7,19 @@ import org.aerovek.chartr.data.model.elrond.address.Address
 import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants
 import org.aerovek.chartr.data.model.elrond.esdt.EsdtSpecialRole
 import org.aerovek.chartr.data.model.elrond.network.NetworkConfig
-import org.aerovek.chartr.data.model.elrond.transaction.
+import org.aerovek.chartr.data.model.elrond.transaction.Transaction
+import org.aerovek.chartr.data.model.elrond.wallet.Wallet
+import org.aerovek.chartr.data.util.toHex
+
+@Deprecated("DO NOT USE!!! This should be converted into its respective repository implementation")
+class SetSpecialRolesEsdtUsecase internal constructor(
+    private val sendTransactionUsecase: SendTransactionUsecase
+) {
+
+    fun execute(
+        account: Account,
+        wallet: Wallet,
+        networkConfig: NetworkConfig,
+        gasPrice: Long,
+        tokenIdentifier: String,
+        
