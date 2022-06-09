397a73d4eeed74d61f9e4deaa2952b12b95a3527
makeline <111859525+makeline@users.noreply.github.com>
2022-06-09 21:05:30 +0000

    Starting from scratch

--- data/src/main/java/org/aerovek/chartr/data/model/ResponseBase.kt
+++ data/src/main/java/org/aerovek/chartr/data/model/ResponseBase.kt
@@ -5,4 +5,18 @@ import org.aerovek.chartr.data.exceptions.RestClientRequestException
 class ResponseBase<T> {
     val data: T? = null
     val error: String? = null
-    val code: 
+    val code: String? = null // ex: "successful"
+
+    @Throws(RestClientRequestException::class)
+    fun throwIfError() {
+        if (!error.isNullOrEmpty()) {
+            throw RestClientRequestException(error)
+        }
+        /*
+        if (code != "successful") {
+            throw RestClientRequestException(code)
+        }
+
+         */
+    }
+}
