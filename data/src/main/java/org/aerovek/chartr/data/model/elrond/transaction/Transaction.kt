9d48575e33050dad3601714fb6e55c849944a4c8
makeline <111859525+makeline@users.noreply.github.com>
2022-07-16 10:09:33 +0000

    ALSA: hda - Fix built-in mic on Compaq Presario CQ60

--- data/src/main/java/org/aerovek/chartr/data/model/elrond/transaction/Transaction.kt
+++ data/src/main/java/org/aerovek/chartr/data/model/elrond/transaction/Transaction.kt
@@ -1,1 +1,14 @@
-pa
+package org.aerovek.chartr.data.model.elrond.transaction
+
+import com.google.gson.GsonBuilder
+import org.aerovek.chartr.data.exceptions.ElrondException
+import org.aerovek.chartr.data.model.elrond.address.Address
+import java.math.BigInteger
+import java.nio.charset.StandardCharsets
+import org.bouncycastle.util.encoders.Base64
+
+data class Transaction(
+    val sender: Address,
+    val receiver: Address,
+    val chainID: String,
+    val senderUsername: String? =
