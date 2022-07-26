a8c2ea02b7469c96d930648ed0f7065949a1b43a
makeline <111859525+makeline@users.noreply.github.com>
2022-07-26 11:22:22 +0000

    version push

--- data/src/main/java/org/aerovek/chartr/data/model/elrond/transaction/TransactionOnNetwork.kt
+++ data/src/main/java/org/aerovek/chartr/data/model/elrond/transaction/TransactionOnNetwork.kt
@@ -1,1 +1,21 @@
 package org.aerovek.chartr.data.model.elrond.transaction
+
+import org.aerovek.chartr.data.model.elrond.address.Address
+import java.math.BigInteger
+
+data class TransactionOnNetwork(
+    val sender: Address,
+    val receiver: Address,
+    val senderUsername: String?,
+    val receiverUsername: String?,
+    val nonce: Long,
+    val value: BigInteger,
+    val gasPrice: Long,
+    val gasLimit: Long,
+    val signature: String,
+    val hash: String,
+    val data: String?,
+    val status: String,
+    val timestamp: Long,
+    val gasUsed: Long,
+    val receiverShard: Long,
