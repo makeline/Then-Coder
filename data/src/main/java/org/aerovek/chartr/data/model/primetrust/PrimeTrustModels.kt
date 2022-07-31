7cb91e6da2562b5814c0498c62d27a8ae2fdc1f4
makeline <111859525+makeline@users.noreply.github.com>
2022-07-31 22:24:20 +0000

    Sync commit

--- data/src/main/java/org/aerovek/chartr/data/model/primetrust/PrimeTrustModels.kt
+++ data/src/main/java/org/aerovek/chartr/data/model/primetrust/PrimeTrustModels.kt
@@ -1,4 +1,31 @@
 package org.aerovek.chartr.data.model.primetrust
 
 import com.google.gson.annotations.JsonAdapter
-import com.google.gson.ann
+import com.google.gson.annotations.SerializedName
+
+
+//region $CreateUser
+
+data class PTCreateUserData(
+    val data: PTUser
+)
+
+data class PTUser(
+    val type: String,
+    val attributes: PTCreateUserAttributes
+)
+
+data class PTCreateUserAttributes(
+    val email: String,
+    val name: String,
+    val password: String
+)
+
+data class PTCreateUserResponseData(
+    val data: PTCreateUserResponse
+)
+
+data class PTCreateUserResponse(
+    val type: String,
+    val id: String,
+    val attributes: PTCreateUserRespon
