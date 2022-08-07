bcaf3e28ef7cbe7f8d839db37accf652bc62daed
makeline <111859525+makeline@users.noreply.github.com>
2022-08-07 17:42:17 +0000

    Services squares

--- data/src/main/java/org/aerovek/chartr/data/network/ElrondNetwork.kt
+++ data/src/main/java/org/aerovek/chartr/data/network/ElrondNetwork.kt
@@ -3,4 +3,14 @@ package org.aerovek.chartr.data.network
 import org.aerovek.chartr.data.model.elrond.esdt.EsdtConstants
 
 /**
- * The chosen build variant will u
+ * The chosen build variant will ultimately dictate which URLs are currently active.
+ * If 'productionRelease' is the selected build variant, MainNet will be used,
+ * all other build variants will use the DevNet object.
+ * The scAddress should be the smart contract address that stores account information.
+ * Refer to the createAccount logic to see how that works.
+ */
+sealed class ElrondNetwork(
+    open val apiUrl: String,
+    open val gatewayUrl: String,
+    open val aeroTokenId: String,
+    open val scAddress: Stri
