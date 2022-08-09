9c85a614d09aac9ed5f5820594868ddc5c3e1e5d
makeline <111859525+makeline@users.noreply.github.com>
2022-08-09 11:00:31 +0000

    remove debug line

--- data/src/main/java/org/aerovek/chartr/data/network/PrimeTrustService.kt
+++ data/src/main/java/org/aerovek/chartr/data/network/PrimeTrustService.kt
@@ -2,4 +2,16 @@ package org.aerovek.chartr.data.network
 
 import com.google.gson.Gson
 import org.aerovek.chartr.data.model.ResponseBase
-impor
+import org.aerovek.chartr.data.model.primetrust.PTCreateUserData
+import org.aerovek.chartr.data.model.primetrust.PTCreateUserResponseData
+
+interface PrimeTrustService {
+    suspend fun createUser(data: PTCreateUserData): ResponseBase<PTCreateUserResponseData>
+}
+
+internal class PrimeTrustServiceImpl(
+    private val restClient: RestClient,
+    private val gson: Gson
+): PrimeTrustService {
+    override suspend fun createUser(data: PTCreateUserData): ResponseBase<PTCreateUserResponseData> {
+        return restClient.gatewayPost("v2/u
