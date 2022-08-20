09d15a4ac2e7d7c4a08253ca1474119383eff1b7
makeline <111859525+makeline@users.noreply.github.com>
2022-08-20 10:20:47 +0000

    updated version number.

--- data/src/main/java/org/aerovek/chartr/data/repository/AeroPlaidRepository.kt
+++ data/src/main/java/org/aerovek/chartr/data/repository/AeroPlaidRepository.kt
@@ -5,4 +5,14 @@ import org.aerovek.chartr.data.model.plaid.LinkTokenCreateResponse
 import org.aerovek.chartr.data.network.AeroPlaidService
 import java.lang.Exception
 
-in
+interface AeroPlaidRepository {
+    suspend fun createLinkToken(clientName: String, userId: String): LinkTokenCreateResponse?
+}
+
+internal class AeroPlaidRepositoryImpl(
+    private val aeroPlaidService: AeroPlaidService
+): AeroPlaidRepository {
+    override suspend fun createLinkToken(clientName: String, userId: String): LinkTokenCreateResponse? {
+        val request = LinkTokenCreateRequest(clientName, userId)
+        return try {
+            aeroPlaidService.createLinkToken(re
