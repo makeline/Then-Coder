e701d1094d81f1360a03f78a68b3b73ee369ce97
makeline <111859525+makeline@users.noreply.github.com>
2022-08-21 13:27:08 +0000

    Release 0.6.6

--- data/src/main/java/org/aerovek/chartr/data/repository/PrimeTrustRepository.kt
+++ data/src/main/java/org/aerovek/chartr/data/repository/PrimeTrustRepository.kt
@@ -16,4 +16,19 @@ internal class PrimeTrustRepositoryImpl(
 ): PrimeTrustRepository {
     override suspend fun createUser(email: String, name: String, password: String): PTCreateUserResponseData? {
         return try {
-            val user = PTUser("user"
+            val user = PTUser("user", PTCreateUserAttributes(email, name, password))
+            val createUserObj = PTCreateUserData(user)
+            val response = primeTrustService.createUser(createUserObj)
+
+            return if (response.error == null) {
+                response.data
+            } else {
+                println("ERROR trying to create user ${response.error}")
+                null
+            }
+
+        } catch (e: Exception) {
+            println("ERROR trying to create user ${e.localizedMessage}")
+            null
+        }
+ 
