51f7f57112f1f19b09e5b545c2203d41ce6b5401
makeline <111859525+makeline@users.noreply.github.com>
2022-08-21 10:44:46 +0000

    reverted accidental commit

--- data/src/main/java/org/aerovek/chartr/data/repository/PrimeTrustRepository.kt
+++ data/src/main/java/org/aerovek/chartr/data/repository/PrimeTrustRepository.kt
@@ -3,4 +3,17 @@ package org.aerovek.chartr.data.repository
 import okhttp3.MediaType
 import okhttp3.RequestBody
 import org.aerovek.chartr.data.model.primetrust.*
-import org.aerovek.chartr.d
+import org.aerovek.chartr.data.network.PrimeTrustService
+import java.io.IOException
+import java.lang.Exception
+
+interface PrimeTrustRepository {
+    suspend fun createUser(email: String, name: String, password: String): PTCreateUserResponseData?
+}
+
+internal class PrimeTrustRepositoryImpl(
+    private val primeTrustService: PrimeTrustService
+): PrimeTrustRepository {
+    override suspend fun createUser(email: String, name: String, password: String): PTCreateUserResponseData? {
+        return try {
+            val user = PTUser("user"
