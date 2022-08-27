d4f84f6dfeb405f0115307b377438197ba655c49
makeline <111859525+makeline@users.noreply.github.com>
2022-08-27 22:43:20 +0000

    Update theme version

--- data/src/main/java/org/aerovek/chartr/data/repository/elrond/AccountRepositoryImpl.kt
+++ data/src/main/java/org/aerovek/chartr/data/repository/elrond/AccountRepositoryImpl.kt
@@ -1,4 +1,15 @@
 package org.aerovek.chartr.data.repository.elrond
 
 import org.aerovek.chartr.data.model.elrond.account.Account
-import org.aerovek.chartr.data.model.elrond
+import org.aerovek.chartr.data.model.elrond.address.Address
+import org.aerovek.chartr.data.model.elrond.toDomain
+import org.aerovek.chartr.data.exceptions.ElrondException
+import org.aerovek.chartr.data.model.elrond.account.AccountToken
+import org.aerovek.chartr.data.network.ElrondApiService
+import org.aerovek.chartr.data.network.ElrondGatewayService
+import java.io.IOException
+import java.math.BigInteger
+
+internal class AccountRepositoryImpl(
+    private val elrondGatewayService: ElrondGatewayService,
+    private val elrondApi
