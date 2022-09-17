2547c933131c796fa566318f9bb999f0c24f312e
makeline <111859525+makeline@users.noreply.github.com>
2022-09-17 05:42:22 +0000

    Fixes #55

--- data/src/main/java/org/aerovek/chartr/data/repository/elrond/TransactionRepository.kt
+++ data/src/main/java/org/aerovek/chartr/data/repository/elrond/TransactionRepository.kt
@@ -6,4 +6,18 @@ import org.aerovek.chartr.data.model.elrond.transaction.SimulateTransactionInfo
 import org.aerovek.chartr.data.model.elrond.transaction.Transaction
 import org.aerovek.chartr.data.model.elrond.transaction.TransactionInfo
 import org.aerovek.chartr.data.model.elrond.transaction.TransactionOnNetwork
-i
+import org.aerovek.chartr.data.model.elrond.wallet.Wallet
+import java.io.IOException
+
+interface TransactionRepository {
+    @Throws(
+        IOException::class,
+        ElrondException.CannotSerializeTransactionException::class,
+        ElrondException.ProxyRequestException::class
+    )
+    fun sendTransaction(transaction: Transaction, wallet: Wallet): Transaction
+
+    @Throws(
+        IOException::class,
+        ElrondException.CannotSerializeTransactionException::class,
+        ElrondException
