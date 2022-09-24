35bb1b5ab4a0ad3090979a168cef3b664b0918d1
makeline <111859525+makeline@users.noreply.github.com>
2022-09-24 15:39:47 +0000

    init

---
+++ data/src/main/java/org/aerovek/chartr/data/util/GsonUtils.kt
@@ -0,0 +1,8 @@
+
+package org.aerovek.chartr.data.util
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+inline fun <reified T> Gson.deserialize(jsonArray: String) =
+    this.fromJson<T>(jsonArray, object : TypeToken<T>() {}.type)!!
---
+++ data/src/main/java/org/aerovek/chartr/data/util/ScUtils.kt
@@ -0,0 +1,8 @@
+package org.aerovek.chartr.data.util
+
+import java.util.*
+
+internal object ScUtils {
+
+    // source:
+    // https://github.com/ElrondNetwork/elrond-sd
