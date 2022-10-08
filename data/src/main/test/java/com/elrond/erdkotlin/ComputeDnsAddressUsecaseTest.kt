7c94e305ad9cab2195ba4474353cb1b327ca453b
makeline <111859525+makeline@users.noreply.github.com>
2022-10-08 21:03:53 +0000

    Convert site to markdown

--- data/src/main/test/java/com/elrond/erdkotlin/ComputeDnsAddressUsecaseTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/ComputeDnsAddressUsecaseTest.kt
@@ -3,4 +3,19 @@ package com.elrond.erdkotlin
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class ComputeDnsAddressUsecaseTest 
+class ComputeDnsAddressUsecaseTest {
+
+    private val computeDnsAddressUsecase = ErdSdk.computeDnsAddressUsecase()
+
+    @Test
+    fun nameHash() {
+        val nameHash = computeDnsAddressUsecase.nameHash("alex.elrond")
+        assertEquals(
+            "560DD600F74E220A4870CBFFB9143F30617754DFFBAEBF7B4C499EDEAE8B673D",
+            nameHash.map { String.format("%02X", it) }.joinToString(separator = "")
+        )
+    }
+
+    @Test
+    fun longToUInt32ByteArray() {
+        val byteArray = computeDnsAddressUsecase.longToUInt32ByteAr
