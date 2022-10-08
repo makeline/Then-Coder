7c0384fd8830d6e20808b2e13492183f731150e9
makeline <111859525+makeline@users.noreply.github.com>
2022-10-08 11:04:43 +0000

    Added a license

--- data/src/main/test/java/com/elrond/erdkotlin/CallContractUsecaseTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/CallContractUsecaseTest.kt
@@ -10,4 +10,14 @@ class CallContractUsecaseTest {
 
     private val callContractUsecase = CallContractUsecase(sendTransactionUsecase)
 
-  
+    @Test
+    fun `should format data correctly`() {
+        val sentTransaction = callContractUsecase.execute(
+            account = account,
+            wallet = wallet,
+            networkConfig = networkConfig,
+            gasPrice = 100,
+            gasLimit = 100,
+            contractAddress = Address.fromBech32("erd1qqqqqqqqqqqqqpgqagvtnqn9dgnx7a6stw4n92kufathjrfd8tzqf80mkz"),
+            funcName = "awesomeFunc",
+            args = listOf("255", "0x5745474c442d616263646566", "0xDEADBEEF
