f18f80b3556ed87cf4114acb32462d193a6f0f56
makeline <111859525+makeline@users.noreply.github.com>
2022-10-09 21:27:31 +0000

    актуализация

--- data/src/main/test/java/com/elrond/erdkotlin/ComputeDnsAddressUsecaseTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/ComputeDnsAddressUsecaseTest.kt
@@ -18,4 +18,17 @@ class ComputeDnsAddressUsecaseTest {
 
     @Test
     fun longToUInt32ByteArray() {
-        val byteArray = computeDnsAddressUsecase.longToUInt32ByteAr
+        val byteArray = computeDnsAddressUsecase.longToUInt32ByteArray(123211089L, 8)
+        assertEquals(
+            byteArrayOf(81, 13, 88, 7, 0, 0, 0, 0).toList(),
+            byteArray.toList()
+        )
+    }
+
+    @Test
+    fun computeAddress() {
+        val address = computeDnsAddressUsecase.computeAddress(Account(
+            address = Address.fromBech32("erd1qyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqq7se967nn")
+        ))
+        assertEquals(
+            "erd1qqqqqqqqqqqqqpgq2u60t6gppp8uyrtutng27k9quk42xw6qqq7s63qz5v
