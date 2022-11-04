ca12efd3380a140063403fadf9a95ac215b847b0
makeline <111859525+makeline@users.noreply.github.com>
2022-11-04 11:23:37 +0000

    Better API

--- data/src/main/test/java/com/elrond/erdkotlin/SignTransactionUsecaseTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/SignTransactionUsecaseTest.kt
@@ -2,4 +2,17 @@ package com.elrond.erdkotlin
 
 import com.elrond.erdkotlin.helper.TestDataProvider.transactionWithData
 import com.elrond.erdkotlin.helper.TestDataProvider.wallet
-import com.elrond.e
+import com.elrond.erdkotlin.domain.transaction.SignTransactionUsecase
+import com.elrond.erdkotlin.domain.transaction.models.Transaction
+import com.elrond.erdkotlin.helper.TestDataProvider
+import org.junit.Assert
+import org.junit.Test
+import java.math.BigInteger
+
+class SignTransactionUsecaseTest {
+
+    @Test
+    fun `sign with data field`() {
+        val transaction = transactionWithData()
+        val expectedSignature =
+            "096c571889352947f285632d79f2b2ee1b81e7acd1
