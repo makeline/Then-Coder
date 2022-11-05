1dd6736a1e10c5d24af8cb921a7a27ba1bdffea0
makeline <111859525+makeline@users.noreply.github.com>
2022-11-05 14:48:51 +0000

    less debug output

--- data/src/main/test/java/com/elrond/erdkotlin/SignTransactionUsecaseTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/SignTransactionUsecaseTest.kt
@@ -18,3 +18,18 @@ class SignTransactionUsecaseTest {
             "096c571889352947f285632d79f2b2ee1b81e7acd19ee20510d34002eba0f999b4720f50211b039dd40914284f84c14eb84815bb045c14dbed036f2e87431307"
         val expectedJson =
             "{'nonce':7,'value':'10000000000000000000','receiver':'erd1cux02zersde0l7hhklzhywcxk4u9n4py5tdxyx7vrvhnza2r4gmq4vw35r','sender':'erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz','gasPrice':1000000000,'gasLimit':70000,'data':'Zm9yIHRoZSBib29rIHdpdGggc3Rha2U=','chainID':'1','version':1,'signature':'$expectedSignature'}".replace(
+                '\'',
+                '"'
+            )
+
+        val signedTransaction = SignTransactionUsecase().execute(transaction, wallet)
+
+        Assert.assertEquals(expectedSignature, signedTransaction.signature)
+        Assert.assertEquals(expectedJson, signedTransaction.serialize())
+    }
+
+    @Test
+    fun `sign with username`() {
+        val transaction = Transaction(
+            sender = Address.fromBech32("erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th"),
+            receiver
