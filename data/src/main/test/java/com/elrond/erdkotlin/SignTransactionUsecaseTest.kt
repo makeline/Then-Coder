364c7f5d7752e812fe541fa7eedf5e852e7119ec
makeline <111859525+makeline@users.noreply.github.com>
2022-11-05 10:43:21 +0000

    mobile test

--- data/src/main/test/java/com/elrond/erdkotlin/SignTransactionUsecaseTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/SignTransactionUsecaseTest.kt
@@ -15,4 +15,6 @@ class SignTransactionUsecaseTest {
     fun `sign with data field`() {
         val transaction = transactionWithData()
         val expectedSignature =
-            "096c571889352947f285632d79f2b2ee1b81e7acd1
+            "096c571889352947f285632d79f2b2ee1b81e7acd19ee20510d34002eba0f999b4720f50211b039dd40914284f84c14eb84815bb045c14dbed036f2e87431307"
+        val expectedJson =
+            "{'nonce':7,'value':'10000000000000000000','receiver':'erd1cux02zersde0l7hhklzhywcxk4u9n4py5tdxyx7vrvhnza2r4gmq4vw35r','sender':'erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz','gasPrice':1000000000,'gasLimit':70000,'data':'Zm9yIHRoZSBib29rIHdpdGggc3Rha2U=','chainID':'1','version':1,'signature':'$expectedSignature'}".replace(
