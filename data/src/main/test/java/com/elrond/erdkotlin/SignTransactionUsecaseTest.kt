94d28635470d9e7f0a2ff16813b6a050f1f7237d
makeline <111859525+makeline@users.noreply.github.com>
2022-11-06 22:36:07 +0000

    Fix remaining tests.

--- data/src/main/test/java/com/elrond/erdkotlin/SignTransactionUsecaseTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/SignTransactionUsecaseTest.kt
@@ -66,4 +66,10 @@ class SignTransactionUsecaseTest {
         // Without data field
         val transaction = TestDataProvider.transactionWithoutData()
         val expectedSignature =
-            "4a6d8186eae110894e7417af82c9bf9592696c0600faf110972e0e5310d8485efc656b867a2336acec2b
+            "4a6d8186eae110894e7417af82c9bf9592696c0600faf110972e0e5310d8485efc656b867a2336acec2b4c1e5f76c9cc70ba1803c6a46455ed7f1e2989a90105"
+        val expectedJson =
+            "{'nonce':8,'value':'10000000000000000000','receiver':'erd1cux02zersde0l7hhklzhywcxk4u9n4py5tdxyx7vrvhnza2r4gmq4vw35r','sender':'erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz','gasPrice':1000000000,'gasLimit':50000,'chainID':'1','version':1,'signature':'$expectedSignature'}".replace(
+                '\'',
+                '"'
+            )
+        val signedTransaction = SignTransactionUseca
