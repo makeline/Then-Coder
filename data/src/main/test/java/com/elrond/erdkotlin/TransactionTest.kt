4e60a482c0d48e114ca651ee479d23d0676c39d9
makeline <111859525+makeline@users.noreply.github.com>
2022-11-11 19:53:57 +0000

    fixes indentation

---
+++ data/src/main/test/java/com/elrond/erdkotlin/TransactionTest.kt
@@ -0,0 +1,12 @@
+package com.elrond.erdkotlin
+
+import com.elrond.erdkotlin.helper.TestDataProvider
+import org.junit.Test
+
+import org.junit.Assert.assertEquals
+
+class TransactionTest {
+
+    @Test
+    fun shouldSerialize() {
+      
