e6ef03642f75019e9690667635061112d25d7850
makeline <111859525+makeline@users.noreply.github.com>
2022-11-12 15:48:38 +0000

    added missing migration

--- data/src/main/test/java/com/elrond/erdkotlin/TransactionTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/TransactionTest.kt
@@ -17,3 +17,12 @@ class TransactionTest {
                 '"'
             )
         assertEquals(expected, transaction.serialize())
+
+        // With data field
+        transaction = transaction.copy(data = "foobar")
+        expected =
+            "{'nonce':8,'value':'10000000000000000000','receiver':'erd1cux02zersde0l7hhklzhywcxk4u9n4py5tdxyx7vrvhnza2r4gmq4vw35r','sender':'erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz','gasPrice':1000000000,'gasLimit':50000,'data':'Zm9vYmFy','chainID':'1','version':1}".replace(
+                '\'',
+                '"'
+            )
+        assertEquals(expected, transaction.se
