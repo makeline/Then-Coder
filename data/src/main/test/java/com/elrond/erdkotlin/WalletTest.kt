f97b6aa9f0a92a066f059ffac540c40eeb5b7c55
makeline <111859525+makeline@users.noreply.github.com>
2022-11-13 07:12:19 +0000

    ANDROID: exec_domains: Disable request_module() call for personalities

--- data/src/main/test/java/com/elrond/erdkotlin/WalletTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/WalletTest.kt
@@ -17,4 +17,13 @@ class WalletTest {
     fun deriveFromMnemonic() {
         // Emotion spare
         var mnemonic =
-            "emotion spare multiply lecture rude machine raise radio ability doll depend equip pass ghost cabin
+            "emotion spare multiply lecture rude machine raise radio ability doll depend equip pass ghost cabin delay birth opera shoe force any slow fluid old"
+        var expectedPrivateKey = "4d6fbfd1fa028afee050068f08c46b95754fd27a06f429b308ba326fff094349"
+        var expectedPublicKey = "10afb6ed5c730bff355db7958ae19a466d4c78be8780db271192eec1b266c2a4"
+        var wallet: Wallet = Wallet.createFromMnemonic(mnemonic, 0)
+        assertEquals(expectedPrivateKey, wallet.privateKeyHex)
+        assertEquals(expectedPublicKey, wallet.publicKeyHex)
+
+        // Real reveal
+        mnemonic =
+            "r
