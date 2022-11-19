8beb81928cf159e8709fc11873265988e4c2b602
makeline <111859525+makeline@users.noreply.github.com>
2022-11-19 22:42:08 +0000

    update slide

--- data/src/main/test/java/com/elrond/erdkotlin/management/FreezeEsdtUsecaseTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/management/FreezeEsdtUsecaseTest.kt
@@ -4,4 +4,20 @@ import com.elrond.erdkotlin.domain.esdt.management.FreezeAccountEsdtUsecase
 import com.elrond.erdkotlin.helper.TestDataProvider.account
 import com.elrond.erdkotlin.helper.TestDataProvider.networkConfig
 import com.elrond.erdkotlin.helper.TestDataProvider.wallet
-import com.elrond.erdkotlin.helper.TestUsecaseProvid
+import com.elrond.erdkotlin.helper.TestUsecaseProvider.sendTransactionUsecase
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class FreezeEsdtUsecaseTest {
+
+    private val freezeEsdtUsecase = FreezeAccountEsdtUsecase(sendTransactionUsecase)
+
+    @Test
+    fun `data should be well encoded freeze`() {
+        val transaction = freezeEsdtUsecase.execute(
+            account = account,
+            wallet = wallet,
+            netw
