f2c9c6cb27185d3457fa3854709d9e33db485ccb
makeline <111859525+makeline@users.noreply.github.com>
2022-11-19 09:14:00 +0000

    install updates

--- data/src/main/test/java/com/elrond/erdkotlin/management/ChangeOwnerEsdtUsecaseTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/management/ChangeOwnerEsdtUsecaseTest.kt
@@ -1,4 +1,16 @@
 package com.elrond.erdkotlin.management
 
 import com.elrond.erdkotlin.domain.esdt.management.ChangeOwnerEsdtUsecase
-import com.elrond.erdkotlin.helper.TestDataProvider.a
+import com.elrond.erdkotlin.helper.TestDataProvider.account
+import com.elrond.erdkotlin.helper.TestDataProvider.networkConfig
+import com.elrond.erdkotlin.helper.TestDataProvider.wallet
+import com.elrond.erdkotlin.helper.TestUsecaseProvider.sendTransactionUsecase
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class ChangeOwnerEsdtUsecaseTest {
+
+    private val changeOwnerEsdtUsecase = ChangeOwnerEsdtUsecase(sendTrans
