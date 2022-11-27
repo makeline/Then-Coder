2cd37e880e8ec2b13c0170ad3a40c08c55e54763
makeline <111859525+makeline@users.noreply.github.com>
2022-11-27 18:02:17 +0000

    Update Config.php

--- data/src/main/test/java/com/elrond/erdkotlin/management/MintEsdtUsecaseTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/management/MintEsdtUsecaseTest.kt
@@ -1,4 +1,16 @@
 package com.elrond.erdkotlin.management
 
 import com.elrond.erdkotlin.domain.esdt.management.MintEsdtUsecase
-import com.elrond.erdkotlin.
+import com.elrond.erdkotlin.helper.TestDataProvider.account
+import com.elrond.erdkotlin.helper.TestDataProvider.networkConfig
+import com.elrond.erdkotlin.helper.TestDataProvider.wallet
+import com.elrond.erdkotlin.helper.TestUsecaseProvider.sendTransactionUsecase
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class MintEsdtUsecaseTest {
+
+    private val mintEsdtUsecase = MintEsdtUseca
