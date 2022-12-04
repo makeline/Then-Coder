ab72077f42737cf0cfbd84aa843c54fb63dfce85
makeline <111859525+makeline@users.noreply.github.com>
2022-12-04 08:54:49 +0000

    Update project.clj

--- data/src/main/test/java/com/elrond/erdkotlin/management/SetSpecialRolesToAccountEsdtUsecaseTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/management/SetSpecialRolesToAccountEsdtUsecaseTest.kt
@@ -6,3 +6,18 @@ import com.elrond.erdkotlin.helper.TestDataProvider.networkConfig
 import com.elrond.erdkotlin.helper.TestDataProvider.wallet
 import com.elrond.erdkotlin.helper.TestUsecaseProvider.sendTransactionUsecase
 import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class SetSpecialRolesToAccountEsdtUsecaseTest {
+
+    private val setSpecialRolesEsdtUsecase = SetSpecialRolesEsdtUsecase(sendTransactionUsecase)
+
+    @Test
+    fun `data should be well encoded freeze`() {
+        val transaction = setSpecialRolesEsdtUsecase.execute(
+            account = account,
+            wallet = wallet,
+  
