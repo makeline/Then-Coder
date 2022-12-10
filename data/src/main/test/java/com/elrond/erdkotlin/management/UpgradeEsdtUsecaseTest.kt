af54a675d48397ff01eecb4cfddb7c9eb761454c
makeline <111859525+makeline@users.noreply.github.com>
2022-12-10 19:08:05 +0000

    Linux 3.10.96

--- data/src/main/test/java/com/elrond/erdkotlin/management/UpgradeEsdtUsecaseTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/management/UpgradeEsdtUsecaseTest.kt
@@ -4,4 +4,19 @@ import com.elrond.erdkotlin.domain.esdt.management.UpgradeEsdtUsecase
 import com.elrond.erdkotlin.domain.esdt.models.ManagementProperty
 import com.elrond.erdkotlin.helper.TestDataProvider.account
 import com.elrond.erdkotlin.helper.TestDataProvider.networkConfig
-import com.el
+import com.elrond.erdkotlin.helper.TestDataProvider.wallet
+import com.elrond.erdkotlin.helper.TestUsecaseProvider.sendTransactionUsecase
+import junit.framework.Assert.assertEquals
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class UpgradeEsdtUsecaseTest {
+
+    private val upgradeEsdtUsecase = UpgradeEsdtUsecase(sendTransactionUsecase)
+
+    @Test
+    fun `data should be well encoded`() {
+     
