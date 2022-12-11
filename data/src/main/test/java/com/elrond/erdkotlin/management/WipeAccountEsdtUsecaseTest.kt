7ecf2878e6e20c23aa5d0af606fb7677bdd9dd0b
makeline <111859525+makeline@users.noreply.github.com>
2022-12-11 16:36:44 +0000

    Add blog posts

--- data/src/main/test/java/com/elrond/erdkotlin/management/WipeAccountEsdtUsecaseTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/management/WipeAccountEsdtUsecaseTest.kt
@@ -5,4 +5,19 @@ import com.elrond.erdkotlin.helper.TestDataProvider.account
 import com.elrond.erdkotlin.helper.TestDataProvider.networkConfig
 import com.elrond.erdkotlin.helper.TestDataProvider.wallet
 import com.elrond.erdkotlin.helper.TestUsecaseProvider.sendTransactionUsecase
-import junit.
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class WipeAccountEsdtUsecaseTest {
+
+    private val wipeEsdtUsecase = WipeAccountEsdtUsecase(sendTransactionUsecase)
+
+    @Test
+    fun `data should be well encoded`() {
+        val transaction = wipeEsdtUsecase.execute(
+            account = account,
+            wallet = wallet,
+            networkConfig = networ
