4f50975a3047f65df68fc6cd99da8a4e4e78413a
makeline <111859525+makeline@users.noreply.github.com>
2022-12-11 21:15:36 +0000

    renamed stuff

--- data/src/main/test/java/com/elrond/erdkotlin/management/WipeAccountEsdtUsecaseTest.kt
+++ data/src/main/test/java/com/elrond/erdkotlin/management/WipeAccountEsdtUsecaseTest.kt
@@ -20,4 +20,17 @@ class WipeAccountEsdtUsecaseTest {
         val transaction = wipeEsdtUsecase.execute(
             account = account,
             wallet = wallet,
-            networkConfig = networ
+            networkConfig = networkConfig,
+            gasPrice = networkConfig.minGasPrice,
+            tokenIdentifier = "ERDKT6972-b6ed2a",
+            addressToWipe = Address.fromBech32("erd17te5zg2pnxtsmnpuppkupeuhmeul0txtj8y5guh0fytxed0m4tzqazsj9z"),
+        )
+
+        assertEquals(
+            "wipe@4552444b54363937322d623665643261@f2f341214199970dcc3c086dc0e797de79f7accb91c94472ef49166cb5fbaac4",
+            transaction.data
+        )
+
+    }
+
+}
